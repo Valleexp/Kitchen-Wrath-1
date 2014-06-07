ec15bc70f54eba9e5b817c09b9ef2562ad82e038
SalSatSat <salsatsat@gmail.com>
2014-06-07 17:34:44 +0800

    Added a script to allow GameObjects to follow the main camera. Purpose: Allow for Scrolling script to be used for the other scenes.

---
+++ Assets/Scripts/FollowMainCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMainCamera : MonoBehaviour {
+
+	private GameObject mainCamera = null;
+	
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(mainCamera.GetComponent<MainCamera>().transform.position.x,
+		                                 transform.position.y,
+		                                 transform.position.z);
+	}
+}
--- Assets/Scripts/Scrolling.cs
+++ Assets/Scripts/Scrolling.cs
@@ -4,22 +4,14 @@ using System.Collections;
 public class Scrolling : MonoBehaviour {
 
 	public float scrollingSpeed = 0.0f;
-	private GameObject mainCamera = null;
-
-	void Awake()
-	{
-	}
 
 	// Use this for initialization
 	void Start () {
-		mainCamera = GameObject.Find("Main Camera");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(mainCamera.GetComponent<MainCamera>().transform.position.x,
-		                                 transform.position.y,
-		                                 transform.position.z);
 		Scroll();
 	}
 
